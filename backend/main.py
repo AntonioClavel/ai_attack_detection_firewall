da83f4913123a51e2730988c790d167dd4ae18cd
Antonio <antonio.clavel@alumnos.upm.es>
2026-06-02 11:36:01 +0200

    final version update

--- backend/main.py
+++ backend/main.py
@@ -2,7 +2,6 @@ from fastapi import FastAPI
 import pandas as pd
 import joblib
 import json
-import xgboost as xgb
 import keras
 import numpy as np
 import os
@@ -10,14 +9,15 @@ import os
 app = FastAPI()
 
 scaler = joblib.load('models/scaler.pkl')
+
 with open('models/feature_names.json', 'r') as f:
     feature_names = json.load(f)
-with open('models/mapping.json', 'r') as f:
-    mapping = json.load(f)
+
+with open('models/threshold.json', 'r') as f:
+    threshold_data = json.load(f)
+    best_threshold = threshold_data['binary_threshold']
 
 nn_model = keras.models.load_model('models/nn_model.keras', compile=False)
-xgb_model = xgb.Booster()
-xgb_model.load_model('models/xgb_model.json')
 
 @app.post("/predict")
 def predict(data: dict):
@@ -28,23 +28,9 @@ def predict(data: dict):
     
     nn_prob = nn_model.predict(X_scaled, verbose=0)[0][0]
     
-    # We use 0.05 as our threshold (reasoning is in the NN training file)
-    is_attack = nn_prob > 0.05
+    is_attack = nn_prob > best_threshold
     nn_label = "Attack Detected" if is_attack else "Normal/Benign"
     
-    xgb_label = "N/A (Normal Traffic)"
-    
-    # --- XGBOOST ---
-    if is_attack:
-        dmatrix = xgb.DMatrix(X_scaled, feature_names=feature_names)
-        xgb_preds = xgb_model.predict(dmatrix)
-        
-        xgb_output_idx = np.argmax(xgb_preds, axis=1)[0]
-        
-        real_class_id = xgb_output_idx + 1
-        
-        xgb_label = mapping.get(str(real_class_id), f"Unknown Attack ({real_class_id})")
     return {
-        "nn_prediction": nn_label,
-        "xgb_prediction": xgb_label
+        "prediction": nn_label
     }
--- frontend/ui.py
+++ frontend/ui.py
@@ -13,6 +13,7 @@ st.write("Launch a number of desired attacks randomly selected from the CIC-UNSW
 
 st.sidebar.header("Testing Parameters")
 num_test = st.sidebar.number_input("Number of connections to analyze", 5, 200, 20)
+
 if st.sidebar.button("Launch test"):
     try:
         df_data = pd.read_csv("Dataset/Data.csv")
@@ -36,8 +37,7 @@ if st.sidebar.button("Launch test"):
             results.append({
                 "Row": idx,
                 "Real Classification": real_label,
-                "NN Prediction": resp['nn_prediction'],
-                "XGB Prediction": resp['xgb_prediction']
+                "AI Prediction": resp['prediction']
             })
             
             prog.progress((i + 1) / num_test)
@@ -45,41 +45,20 @@ if st.sidebar.button("Launch test"):
         df_res = pd.DataFrame(results)
         
         st.subheader("Analysis Results")
-        col_a, col_b = st.columns(2)
         
-        with col_a:
-            st.markdown("**Real Attacks Distribution**")
-            
-            df_pie = df_res.copy()
-            counts = df_pie['Real Classification'].value_counts()
-            df_pie['Legend Label'] = df_pie['Real Classification'].apply(lambda x: f"{x} ({counts[x]})")
-            
-            fig1 = px.pie(df_pie, names='Legend Label', hole=.3, color_discrete_sequence=px.colors.sequential.RdBu)
-            
-            st.plotly_chart(fig1, use_container_width=True)
-            
-        with col_b:
-            st.markdown("**XGBoost Model Attack Prediction**")
-            
-            attack_data = df_res[df_res['XGB Prediction'] != 'N/A (Normal Traffic)']
-            
-            if not attack_data.empty:
-                attack_counts = attack_data['XGB Prediction'].value_counts().reset_index()
-                attack_counts.columns = ['Attack Type', 'Count']
-                
-                fig2 = px.bar(attack_counts, x='Attack Type', y='Count', 
-                              color='Attack Type', text='Count')
-                
-                fig2.update_layout(showlegend=False)
-                fig2.update_yaxes(dtick=1)
-                
-                st.plotly_chart(fig2, use_container_width=True)
-            else:
-                st.info("No attacks detected by XGBoost in this sample.")
-
+        st.markdown("**Real Attacks Distribution sent to the Firewall**")
+        
+        df_pie = df_res.copy()
+        counts = df_pie['Real Classification'].value_counts()
+        df_pie['Legend Label'] = df_pie['Real Classification'].apply(lambda x: f"{x} ({counts[x]})")
+        
+        fig1 = px.pie(df_pie, names='Legend Label', hole=.3, color_discrete_sequence=px.colors.sequential.RdBu)
+        st.plotly_chart(fig1, use_container_width=True)
+        
         st.subheader("Details of every line:")
+        
         def color_rows(row):
-            if row['NN Prediction'] == 'Attack Detected':
+            if row['AI Prediction'] == 'Attack Detected':
                 return ['background-color: #fdb9b9'] * len(row)
             return [''] * len(row)
 
